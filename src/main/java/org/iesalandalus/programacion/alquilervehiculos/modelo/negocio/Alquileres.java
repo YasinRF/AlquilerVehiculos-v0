541f174bf226fc2009ee732be17666ecbe8e975b
Yasin <Yasin@DESKTOP-CDLTI0A>
2023-02-21 12:25:02 +0100

    Creamos la clase Alquileres

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Alquileres.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Alquileres.java
@@ -1,7 +1,10 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
 
 import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
@@ -40,11 +43,73 @@ public class Alquileres {
 		}
 		return lista;
 	}
-	
+
 	public int getCantidad() {
 		return coleccionAlquileres.size();
 	}
+
+	private void comprobarAlquiler(Cliente cliente, Turismo turismo, LocalDate fechaAlquiler)
+			throws OperationNotSupportedException {
+
+		for (Alquiler alquiler : get(cliente)) {
+			if (alquiler.getFechaDevolucion() == null) {
+				throw new OperationNotSupportedException("ERROR: El cliente tiene otro alquiler sin devolver.");
+			}
+			if (alquiler.getFechaDevolucion().isAfter(fechaAlquiler)) {
+				throw new OperationNotSupportedException("ERROR: El cliente tiene un alquiler posterior.");
+			}
+		}
+
+		for (Alquiler alquiler : get(turismo)) {
+			if (alquiler.getFechaDevolucion() == null) {
+				throw new OperationNotSupportedException("ERROR: El turismo está actualmente alquilado.");
+			}
+			if (alquiler.getFechaDevolucion().isAfter(fechaAlquiler)) {
+				throw new OperationNotSupportedException("ERROR: El turismo tiene un alquiler posterior.");
+			}
+		}
+
+	}
+
+	public void insertar(Alquiler alquiler) throws OperationNotSupportedException {
+		if (alquiler == null) {
+			throw new NullPointerException("ERROR: No se puede insertar un alquiler nulo.");
+		}
+		comprobarAlquiler(alquiler.getCliente(), alquiler.getTurismo(), alquiler.getFechaAlquiler());
+		coleccionAlquileres.add(alquiler);
+	}
+
+	public void devolver(Alquiler alquiler, LocalDate fechaDevolucion) throws OperationNotSupportedException {
+		if (alquiler == null) {
+			throw new NullPointerException("ERROR: No se puede devolver un alquiler nulo.");
+		}
+		if (!coleccionAlquileres.contains(alquiler)) {
+			throw new OperationNotSupportedException("ERROR: No existe ningún alquiler igual.");
+
+		}
+		alquiler.devolver(fechaDevolucion);
+
+	}
 	
+	public void borrar(Alquiler alquiler) throws OperationNotSupportedException {
+
+		if (alquiler == null) {
+			throw new NullPointerException("ERROR: No se puede borrar un alquiler nulo.");
+		}
+		if (!coleccionAlquileres.contains(alquiler)) {
+			throw new OperationNotSupportedException("ERROR: No existe ningún alquiler igual.");
+		}
+		coleccionAlquileres.remove(alquiler);
+	}
 	
+	public Alquiler buscar(Alquiler alquiler) {
+		if (alquiler == null) {
+			throw new NullPointerException("ERROR: No se puede buscar un alquiler nulo.");
+		}
+		if (coleccionAlquileres.contains(alquiler)) {
+			return alquiler;
+		}
+		return null;
+	}
 
 }
